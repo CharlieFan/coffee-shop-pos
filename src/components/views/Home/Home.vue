83efef38664ea4723b7a66505c19ac6c102ebdd4
Charlie Fan <chen.fan1991@outlook.com>
2018-02-02 17:07:08 -0500

    init home menu

--- src/components/views/Home/Home.vue
+++ src/components/views/Home/Home.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="home-view">
         <header>
-            <h1>Coffee Menu</h1>
-            <div>
+            <h1 class="flex-1">Coffee Menu</h1>
+            <a class="cart-wrapper flex-1"
+                href="javascript:void(0)">
                 <i class="fas fa-shopping-cart"></i>
-            </div>
+                <span>My orders ({{0}})</span>
+            </a>
         </header>
-        <p>What would you like to drink?</p>
+        <p>Welcome what would you like to drink?</p>
 
         <div class="list-wrapper">
             <h3>Espresso Drink</h3>
