9dc7ccd11e5e810f9b67c7009a824681434c8962
Chen Fan <vincebox9111@gmail.com>
2018-02-03 21:37:27 -0500

    style updated

--- src/components/views/CoffeeDetails/CoffeeDetails.vue
+++ src/components/views/CoffeeDetails/CoffeeDetails.vue
@@ -6,10 +6,50 @@
             <div class="form-item">
                 <label>Size:</label>
                 <select class="menu-select">
-                    <option value="s">s</option>
-                    <option value="m">m</option>
-                    <option value="l">l</option>
+                    <option value="s">S</option>
+                    <option value="m">M</option>
+                    <option value="l">L</option>
                 </select>
+            </div>
+
+            <div class="form-item">
+                <label>Espresso:</label>
+                <input class="input-box" type="number" min="1">
+                <i class="fas fa-times"></i>
+                <span>shot(s)</span>
+            </div>
+
+            <div class="form-item">
+                <label>Sweetener:</label>
+                <select class="menu-select">
+                    <option value="s">Sugar</option>
+                    <option value="m">Splanda</option>
+                    <option value="l">Honey</option>
+                </select>
+                <i class="fas fa-times"></i>
+                <input class="input-box" type="number" min="0"><span>shot(s)</span>
+            </div>
+
+            <div class="form-item">
+                <label>Milk:</label>
+                <select class="menu-select">
+                    <option value="m">18% Cream</option>
+                    <option value="s">2% Milk</option>
+                    <option value="l">Skimmed Milk</option>
+                </select>
+                <i class="fas fa-times"></i>
+                <input class="input-box" type="number" min="0"><span>shot(s)</span>
+            </div>
+
+            <div class="form-item">
+                <label>Flavor:</label>
+                <select class="menu-select">
+                    <option value="m">Vanilla</option>
+                    <option value="s">Hazelnut</option>
+                    <option value="l">Caramel</option>
+                </select>
+                <i class="fas fa-times"></i>
+                <input class="input-box" type="number" min="0"><span>shot(s)</span>
             </div>
 
         </form>
