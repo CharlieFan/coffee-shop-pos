afe6c3249dd4f0fac3e31e6738d9a56ff6110e36
Charlie Fan <chen.fan1991@outlook.com>
2018-02-06 17:23:15 -0500

    init ajax module

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+import {get, post} from './http'
+
+export default {
+    menu: {
+        /**
+         * Get Coffee Menu
+         */
+        getCoffeeMenu() {
+            console.log('get menu now')
+        }
+    }
+}
